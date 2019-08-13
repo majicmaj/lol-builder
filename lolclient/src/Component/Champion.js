591833844e651cc756a7b021173257b927e8859c
KeeneWong <keenewonghofung@gmail.com>
2019-08-13 16:20:45 -0400

    the nav bar

---
+++ lolclient/src/Component/Champion.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Champion extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Champion Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Champion;
