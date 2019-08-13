421a85cd410be01e369f92847cbddab9030a5025
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-13 15:55:49 -0400

    seed.js added

---
+++ server/db/seed.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+const championModel = require('../models/Champion')
+
+const url = {
+    v: 'https://ddragon.leagueoflegends.com/realms/na.json',
+    head: 'http://ddragon.leagueoflegends.com/cdn/',
+    tail: '/data/en_US/champion.json',
+    icon: 'http://ddragon.leagueoflegends.com/cdn/9.16.1/img/champion/'
+}
+
+// Get latest game version
+axios.get(url.v)
+
+    .then(r => url.body = r.data.n.champion)
+
+    // Get champion data
+    .then(r => {
+        axios.get(url.head + url.body + url.tail)
+
+            .then(r => { return r.data.data })
+            .then(champions => {
+                let data = []
+                let i = 0
+                Object.keys(champions).forEach(c => {
+                    data[i] = {
+                        name: champions[c].name,
+                        title: champions[c].title,
+                        blurb: champions[c].blurb,
+                        icon: url.icon + champions[c].image.full,
+                        attack: champions[c].info.attack,
+                        defense: champions[c].info.defense,
+                        magic:  champions[c].info.magic,
+                        tags: champions[c].tags
+                    }
+                    i++
+                })
+                return data
+            })
+
+            .then(champions => championModel.deleteMany()
+                .then(() => championModel.create(champions))
+                .then(r => console.log(r))
+            )
+    })
