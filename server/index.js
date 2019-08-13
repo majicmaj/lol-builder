cba544b4660495cbb885a7cd44a29408cc96d758
KeeneWong <keenewonghofung@gmail.com>
2019-08-13 18:44:35 -0400

    use cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const parser = require('body-parser')
-const app = express()
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+const cors = require("cors");
 
-app.use(parser.json())
+app.use(parser.json());
+app.use(cors());
 
-app.use('/', require('./routes/champion'))
-app.use('/champion', require('./routes/champion'))
-app.use('/team', require('./routes/team'))
+app.use("/", require("./routes/champion"));
+app.use("/champion", require("./routes/champion"));
+app.use("/team", require("./routes/team"));
 
-app.set('port', process.env.PORT || 8080)
+app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get('port'), () => console.log(`✅ PORT: ${app.get("port")} 🌟`))
+app.listen(app.get("port"), () =>
+  console.log(`✅ PORT: ${app.get("port")} 🌟`)
+);
