129bf9827cc3963a450bc12247beb8be17096a38
KeeneWong <keenewonghofung@gmail.com>
2019-08-14 12:31:14 -0400

    show team

---
+++ lolclient/src/Component/ShowTeam.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class ShowTeam extends Component {
+  findicon = champid => {
+    this.props.champ.filter(each => {
+      return each._id === champid;
+    });
+  };
+  render() {
+    let teams;
+    if (this.props.team !== []) {
+      teams = this.props.team.map(each => {
+        return (
+          <div key={each.name} className="eachteam test">
+            <h3>{each.name}</h3>
+            <p>Top:{each.top}</p>
+            {/* <p>{this.findicon(each.top)[0].name}</p> */}
+            <p>Jun:{each.jun}</p>
+            <p>Mid:{each.mid}</p>
+            <p>Bot:{each.bot}</p>
+            <p>Sup:{each.sup}</p>
+          </div>
+        );
+      });
+    }
+
+    return (
+      <div className="showteam">
+        <h1>show all team</h1>
+        {teams}
+      </div>
+    );
+  }
+}
+
+export default ShowTeam;
