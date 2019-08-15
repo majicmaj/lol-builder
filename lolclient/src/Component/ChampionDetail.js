008d83634a53834b0262fcc18e48ec39b59c719b
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-15 11:27:48 -0400

    added more stats

--- lolclient/src/Component/ChampionDetail.js
+++ lolclient/src/Component/ChampionDetail.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import "../App.css";
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css';
 
 class ChampionDetail extends Component {
   render() {
     let thischamp = this.props.champ.filter(eachchamp => {
       return eachchamp.name === this.props.match.params.championName;
-    });
+    }); 
+    const data = [
+      {
+        data: {
+          attack: thischamp[0].attack / 9,
+          defense: thischamp[0].defense / 9,
+          magic: thischamp[0].magic / 9,
+        },
+        meta: { color: '#0df' }
+      }
+    ];
+
+    const captions = {
+      // columns
+      attack: 'Attack',
+      defense: 'Defense',
+      magic: 'Magic',
+    };
     // console.log(thischamp);
     return (
       <div class="championdetailmain">
@@ -18,9 +37,13 @@ class ChampionDetail extends Component {
           <h2>{thischamp[0].name}</h2>
           <h3>{thischamp[0].title}</h3>
           <h3>{thischamp[0].blurb}</h3>
-          <h3>attack: {thischamp[0].attack}</h3>
-          <h3>defense: {thischamp[0].defense}</h3>
-          <h3>magic: {thischamp[0].magic}</h3>
+          <RadarChart
+            captions={captions}
+            data={data}
+            size={300}
+            scales={3}
+            className='chart'
+          />
           <svg height="300" width="300">
             <polygon points="200,0 100,173 200,0" />
           </svg>
