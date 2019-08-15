62448892f8b71ea6d528b363f0c8f1e7c96bd6f1
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-15 10:45:01 -0400

    chart

--- lolclient/src/Component/ShowTeam.js
+++ lolclient/src/Component/ShowTeam.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css';
 import "../App.css";
 
 class ShowTeam extends Component {
@@ -9,7 +11,6 @@ class ShowTeam extends Component {
       console.log('this is props')
       console.log(this.props)
       teams = this.props.team.map(each => {
-        // console.log(this.props.champ);
         let topchamp = this.props.champ.filter(
           eachchamp => eachchamp._id === each.top
         );
@@ -24,19 +25,46 @@ class ShowTeam extends Component {
         );
         let supchamp = this.props.champ.filter(
           eachchamp => eachchamp._id === each.sup
-        );
+        ); 
+        const data = [
+          {
+            data: {
+              attack: each.attack / 50,
+              defense: each.defense / 50,
+              magic: each.magic / 50,
+            },
+            meta: { color: '#0df' }
+          }
+        ];
+
+        const captions = {
+          // columns
+          attack: 'Attack',
+          defense: 'Defense',
+          magic: 'Magic',
+        };
 
         return (
           <div key={each._id} className="eachteam">
-            <h3>Team Name: {each.name}</h3>
-            <div class="teamiconcontain">
-              {" "}
-              <img class="teamicons" data-role="top" src={topchamp[0].icon} />
-              <img class="teamicons" src={jgchamp[0].icon} />
-              <img class="teamicons" src={midchamp[0].icon} />
-              <img class="teamicons" src={adcchamp[0].icon} />
-              <img class="teamicons" src={supchamp[0].icon} />
+            <h3>{each.name}</h3>
+            <div className="teamiconcontain">
+              <img className="teamicons" data-role="top" src={topchamp[0].icon} />
+              <img className="teamicons" src={jgchamp[0].icon} />
+              <img className="teamicons" src={midchamp[0].icon} />
+              <img className="teamicons" src={adcchamp[0].icon} />
+              <img className="teamicons" src={supchamp[0].icon} />
+              <div className='button'>
+                <button className='lol-style' >Edit</button>
+                <button className='lol-style'>Delete</button>
+              </div>
             </div>
+            <RadarChart
+              captions={captions}
+              data={data}
+              size={300}
+              scales={3}
+              className='chart'
+            />
           </div>
         );
       });
--- lolclient/src/Component/Team.js
+++ lolclient/src/Component/Team.js
@@ -159,6 +159,8 @@ class Team extends Component {
         <div className="topmain">
           <h1>Create a team</h1>
           <div className="teamtop">
+          </div>
+          <div className="teamtop">
             <div
               className="eachicon"
               style={{
