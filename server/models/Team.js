0f9a2eb29c76dc2ec51fe46a73e2bb51db8c086b
Majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-07-14 14:02:07 -0400

    Added creation date to API

--- server/models/Team.js
+++ server/models/Team.js
@@ -1,36 +1,37 @@
-const mongoose = require('../db/connection')
+const mongoose = require("../db/connection");
 
 const TeamSchema = new mongoose.Schema({
-    name: String,
-    attack: Number,
-    defense: Number,
-    magic: Number,
-    top: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Champion'
-    },
-    mid: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Champion'
-    },
-    jun: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Champion'
-    },
-    bot: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Champion'
-    }, 
-    sup: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Champion'
-    },
-    champions : [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Champion'
-        }
-    ]
-})
+  name: String,
+  creation: String,
+  attack: Number,
+  defense: Number,
+  magic: Number,
+  top: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Champion"
+  },
+  mid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Champion"
+  },
+  jun: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Champion"
+  },
+  bot: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Champion"
+  },
+  sup: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Champion"
+  },
+  champions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Champion"
+    }
+  ]
+});
 
-module.exports = mongoose.model("Team", TeamSchema)
+module.exports = mongoose.model("Team", TeamSchema);
